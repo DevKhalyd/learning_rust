7bbaba4155031e10bef9f720e96778cb06e53ca2
Rolando Garcia <jrdosmil0@gmail.com>
2021-12-06 21:39:39 -0600

    Ready

---
+++ lib/hello_world.rs
@@ -0,0 +1,8 @@
+fn main(){
+    // More info: https://doc.rust-lang.org/book/ch01-02-hello-world.html
+    // Statements here are executed when 
+    // the compiled binary is called
+    // Print text to the console
+    println!("Hello, world!");
+
+}
