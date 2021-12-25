b4bf5d9d8fba08b22f62cb5e234bd0811be1245b
Rolando Garcia <jrdosmil0@gmail.com>
2021-12-24 20:11:44 -0600

    Ready

--- lib/custom_types/structures.rs
+++ lib/custom_types/structures.rs
@@ -1,1 +1,7 @@
 // Ref: https://doc.rust-lang.org/stable/rust-by-example/custom_types/structs.html
+
+/*
+
+There are three types of structures ("structs") that can be created using the struct keyword:
+
+*/
