0210a9c3395e7db4c994736eea5a8fb033b8e720
Rolando Garcia <jrdosmil0@gmail.com>
2022-01-06 10:02:35 -0600

    New problem added

--- lib/primitives/tuples.rs
+++ lib/primitives/tuples.rs
@@ -6,7 +6,7 @@ Functions can use tuples to return multiple values, as tuples can hold any numbe
 
 
 */
-// TODO: Resolve the problem
+// TODO: Compile the errors
 
 // Tuples can be used as function arguments and as return values
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
@@ -62,3 +62,35 @@ fn main() {
     println!("{:?}", matrix);
 
 }
+
+/*
+
+Activity
+
+Recap: Add the fmt::Display trait to the Matrix struct 
+in the above example, so that if you switch from printing 
+the debug format {:?} to the display format {}, you see the 
+following output:
+
+( 1.1 1.2 )
+( 2.1 2.2 )
+
+Add a transpose function using the reverse function as a template, which accepts a matrix as an argument, and returns a matrix in which two elements have been swapped. 
+For example:
+
+println!("Matrix:\n{}", matrix);
+println!("Transpose:\n{}", transpose(matrix));
+
+Output:
+
+Matrix:
+( 1.1 1.2 )
+( 2.1 2.2 )
+Transpose:
+( 1.1 2.1 )
+( 1.2 2.2 )
+
+
+*/
+
+
